309ab380cc9e67d75f6928d24e0f50fdd3fb64cd
Denis <normal64@mail.ru>
2023-09-17 13:16:22 +0200

    fixLclean up

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,22 +1,15 @@
-// const { expect } = require('@wdio/globals')
-// const LoginPage = require('../pageobjects/login.page')
-// const SecurePage = require('../pageobjects/secure.page')
 
 describe('My Login application', () => {
     const emailValue = "test@gmail.com"
     beforeEach(() => {
-        
         browser.url("https://demoqa.com/automation-practice-form")
-        
-    });
+        });
     
     it("should open Practice Form web page", async ()=>{
-        
         browser.setWindowSize(1920, 1600)
         expect(browser).toHaveTitle("DEMOQA")
     })
     it("should fill the form",async ()=>{
-        
         browser.setWindowSize(1920, 1600)
         const emailInput = await $('//*[@id="userEmail"]')
         emailInput.setValue(emailValue)
@@ -25,13 +18,11 @@ describe('My Login application', () => {
     it("should open check if button is clickable ", async ()=>{
         browser.setWindowSize(1920, 1600)
         const elementsButton = await $('#app > div > div > div.row > div:nth-child(1) > div > div > div:nth-child(1) > span > div')
-        
         await expect(elementsButton).toBeClickable()
     })
     it("last name input should exist", async ()=>{
         browser.setWindowSize(1920, 1600)
         const lastNameInput = await $('#lastName')
-        
         await expect(lastNameInput).toBeExisting()
     })
 
