cc5336d6ca91c60e77c7c6ea69042910f5e3e8fd
Denis <normal64@mail.ru>
2023-10-10 22:01:48 +0200

    Resolve conflicts and update ui_tests branch

--- test/specs/phptravels.e2e.js
+++ test/specs/phptravels.e2e.js
@@ -1,68 +1,62 @@
-const travelPage = require("../pageobjects/phptravels.page.js")
-
-browser.addCommand("waitAndClick", function (selector) {
-    // `this` is the return value of $(selector)
-    console.log(`Waiting for element with selector: ${selector}`);
-    const element = $(selector);
-    element.waitForDisplayed();
-    element.waitForClickable({ timeout: 3000 })
-    console.log("Element displayed, clicking...");
-    element.click();
+browser.addCommand("waitAndClick", async function (selector, expectedUrl) {
+  const element = $(selector);
+  element.waitForDisplayed();
+  element.waitForClickable({ timeout: 3000 });
+  element.click();
+  const currentUrl = await browser.getUrl();
+  expect(currentUrl).toEqual(expectedUrl);
 });
 //https://docs.google.com/spreadsheets/d/1kFkc7CKAPNFUw_gajtFJwmSVhJUcVjEuPNESeR4pxuI/edit#gid=0
 
-describe('php travels cases', () => {
-    const emailValue = "testUPDATED@gmail.com"
-    let screenshotIndex = 1;
-    
-    beforeEach(() => {
-        browser.url("https://phptravels.org/login");
-        browser.setWindowSize(1920, 1600)
-        });
-    afterEach(() => {
-        const screenshotName = `test/screenshots/screenshot${screenshotIndex}.png`;
-        browser.saveScreenshot(screenshotName);
-        screenshotIndex++;
-    });
-    
-    it("should fill email input", async ()=>{
-        const isEmailInputExisting = await travelPage.isEmailInputExisting();
-        expect(isEmailInputExisting).toBe(true)
-        await travelPage.enterEmail(emailValue)
-    });
+describe("php travels cases", () => {
+  const emailValue = "test@gmail.com";
 
-    it("should use custom command", async () => {
-        await travelPage.enterEmail(emailValue)
-        console.log("Before waiting and clicking...");
-        await browser.waitAndClick("#login");
-        //how to deal with captcha? cuz technically i am a robot:P
-        //await browser.waitAndClick("#recaptcha-anchor");
-        await browser.pause(1000);
-    });
-    it("icon show reveal password", async () => {
-        const passwordData =  "123456"
-        await travelPage.enterPassword(passwordData);
-        const inputValue = await travelPage.getPasswordInputValue();
-        expect(inputValue).toEqual(passwordData)
-        await travelPage.clickRevealPasswordButton()
-        await browser.pause(1000);
-    });
-    it("forgot password with incorrect email", async () => {
-        await $('#main-body > div > div.row > div > form > div > div.card-body.px-sm-5.py-5 > div.form-group.mb-4.focused > div.d-flex.align-items-center.justify-content-between > div > a').click()
-        let formHeader = await $("#main-body > div > div.row > div > div > div > div > div > h6");
-        await expect(formHeader).toHaveText("Lost Password Reset")
-        await $("#inputEmail").setValue("777");
-        await browser.pause(1000);
-        await browser.keys("\uE007")
-        //how to check pop up pn form validation?
-        //const validationMessageValue = await $("#inputEmail").getAttribute("validationMessage")
-        const validationMessageValue = await browser.execute(() => {
-            const inputElement = document.querySelector("#inputEmail");
-            return inputElement ? inputElement.validationMessage : null;
-        });
-        console.log(`validationMessageValue`, validationMessageValue);
-        await expect(validationMessageValue.length).toBeGreaterThan(0);
-    });
+  beforeEach(() => {
+    browser.url("https://phptravels.org/login");
+    browser.setWindowSize(1920, 1600);
+  });
 
-})
+  it("should fill email input", async () => {
+    const emailInput = await $("#inputEmail");
+    await expect(emailInput).toBeExisting();
+    await emailInput.setValue(emailValue);
+  });
 
+  it("should use custom command", async () => {
+    const emailInput = await $("#inputEmail");
+    await expect(emailInput).toBeExisting();
+    await emailInput.setValue(emailValue);
+    const frame = await $('iframe[title="reCAPTCHA"]');
+    await browser.switchToFrame(frame);
+    const checkbox = await $("span[role='checkbox']");
+    await checkbox.click();
+    await browser.waitAndClick("#login", "https://phptravels.org/login");
+    // how to deal with captcha? cuz technically i am a robot:P
+    // await browser.waitAndClick("#recaptcha-anchor");
+  });
+  it("icon show reveal password", async () => {
+    const passwordData = "123456";
+    const passwordInput = await $("#inputPassword");
+    await passwordInput.setValue(passwordData);
+    expect(passwordInput).toHaveValue(passwordData);
+    await $(
+      '//*[@role="form"]//*[@class="input-group-append"]//button'
+    ).click();
+  });
+  it("forgot password with incorrect email", async () => {
+    await $('//*[@role="form"]//*[@class="mb-2"]//a').click();
+    const formHeader = await $(
+      "//*[@id='main-body']//*[@class='mb-4']//*[@class='h3']"
+    );
+    await expect(formHeader).toHaveText("Lost Password Reset");
+    await $("#inputEmail").setValue("777");
+    await browser.pause(1000);
+    await browser.keys("\uE007");
+
+    const validationMessageValue = await browser.execute(() => {
+      const inputElement = document.querySelector("#inputEmail");
+      return inputElement ? inputElement.validationMessage : null;
+    });
+    await expect(validationMessageValue.length).toBeGreaterThan(0);
+  });
+});
